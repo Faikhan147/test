47b5dc3e08946e2b481e92717a530f59c12e9dd3
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-24 18:09:35 +0530

    Update main.tf

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -1,6 +1,6 @@
 resource "aws_kms_key" "this" {
   deletion_window_in_days = 10
-  description             = "${var.environment}-kms-key"
+  description             = "${var.env}-kms-key"
   vpc_id      = var.vpc_id
 
   policy = jsonencode({
