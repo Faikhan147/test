3119457685ef72dc60748b31f9740022ff6004be
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-24 16:22:46 +0530

    Create main.tf

---
+++ environment/prod/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "kms" {
+  source      = "../../modules/kms"
+  environment = "prod"
+}
+
+module "iam" {
+  source      = "../../modules/iam"
+  environment = "prod"
+}
+
+module "sg" {
+  source      = "../../modules/security-group"
+  environment = "prod"
+  vpc_id      = var.vpc_id
+}
