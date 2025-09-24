53db4903bc769cfca017a021e2eabad4cc27c285
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-25 01:57:45 +0530

    Create backend.tf

---
+++ environment/qa/backend.tf
@@ -0,0 +1,11 @@
+# backend.tf (real-time companies style)
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-backend-all-env"
+    key            = "eks/values/qa/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks-values-qa"
+  }
+}
