6d2f2bb30a897ae3acfd60c9bb9f646fffc2a199
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-25 02:01:03 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-backend-all-env"
-    key            = "eks/${terraform.workspace}/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-locks-values"
-  }
-}
