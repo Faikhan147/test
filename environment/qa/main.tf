a804c2549ffe50f57d8b58daf526968b247e1127
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-24 19:21:23 +0530

    Update main.tf

--- environment/qa/main.tf
+++ environment/qa/main.tf
@@ -1,12 +1,14 @@
 module "kms" {
   source      = "../../modules/kms"
   env         = var.env
-  vpc_id      = var.vpc_id}
+  vpc_id      = var.vpc_id
+}
 
 module "iam" {
   source      = "../../modules/iam"
   env         = var.env
-  vpc_id      = var.vpc_id}
+  vpc_id      = var.vpc_id
+}
 
 module "sg" {
   source      = "../../modules/security-group"
