3d77801a7e84ab81136106fbd7028f85b15c5607
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-28 04:23:46 +0530

    Update outputs.tf

--- environment/staging/outputs.tf
+++ environment/staging/outputs.tf
@@ -1,22 +1,22 @@
 # IAM outputs
 output "iam_cluster_role_arn" {
-  value       = modules.iam.cluster_role_arn
+  value       = module.iam.cluster_role_arn
   description = "ARN of EKS cluster IAM role"
 }
 
 output "iam_node_role_arn" {
-  value       = modules.iam.node_role_arn
+  value       = module.iam.node_role_arn
   description = "ARN of EKS node IAM role"
 }
 
 # KMS outputs
 output "kms_key_arn" {
-  value       = modules.kms.kms_key_arn
+  value       = module.kms.kms_key_arn
   description = "The ARN of the KMS key"
 }
 
 # Security Group outputs
 output "sg_id" {
-  value       = modules.security-group.sg_id
+  value       = module.sg.sg_id
   description = "ID of the security group"
 }
